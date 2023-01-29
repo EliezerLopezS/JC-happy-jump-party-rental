3b986f24bc150933be15bc9ea035ce7e3ed8f874
Eliezer Lopez Sandoval <371eze6.375@gmail.com>
2023-01-28 22:06:28 -0600

    agregue footer nuevo

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer id="myFooter">
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-12 col-sm-6 col-md-3">
+            <img class=" flex-shrink-0 me-3 fit-cover" width="350" height="250" src="Img/logo.jpg">
+            </div>
+            <div class="col-12 col-sm-6 col-md-2">
+                <h5>Location</h5>
+                <ul>
+                <p class="text-muted mb-0">223 Ocean Ave, Lakewood NJ 08701 
+                                </p>
+                    <!--li><a href="#">Home</a></li>
+                    <li><a href="#">Downloads<br /></a></li>
+                    <li><a href="#">Sign Up</a></li>
+                    <li><a href="#">Other Links</a></li-->
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-2">
+                <h5>Email</h5>
+                <ul>
+                 <p class="text-muted mb-0">FernandoJ0220771@gmail.com</p>
+                    <!--li><a href="#">About Us</a></li>
+                    <li><a href="#">Company Information<br /></a></li>
+                    <li><a href="#">Reviews</a></li>
+                    <li><a href="#">Contacts</a></li-->
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-2">
+                <h5>Phone</h5>
+                <ul>
+                <p class="text-muted mb-0">+123456789</p>
+                    <!--li><a href="#">FAQ</a></li>
+                    <li><a href="#">Help Desk<br /></a></li>
+                    <li><a href="#">Forums</a></li>
+                    <li><a href="#">External Links</a></li-->
+                </ul>
+            </div>
+            <div class="col-md-3 social-networks">
+                <div></div><a class="facebook" href="https://www.facebook.com/profile.php?id=100082319887289&mibextid=LQQJ4d"><i class="fa fa-facebook"></i></a><a class="google" href="https://g.co/kgs/bkdM2p"><i class="fa fa-google-plus"></i></a><a class="twitter" href="mailto:FernandoJ0220771@gmail.com"><i class="bi bi-envelope"></i></a><a href="contacts.php"><button class="btn btn-primary" style="margin-top: 20px;" type="button" >Contact us</button></a>
+            </div>
+        </div>
+        <div class="row footer-copyright">
+            <div class="col">
+                <p>© 2023 JC Happy jump party rental. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+    
---
+++ links.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>JC Happy Jump Party Rental</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/Bold-BS4-Footer-Big-Logo.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/Features-Image-icons.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/Features-Image-images.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/bootstrap/css/Carousel-Hero.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
