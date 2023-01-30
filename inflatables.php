7d18391926db031bfd7f80b723f75dd4c84836c8
oswaldo5 <siete.o2r@gmail.com>
2023-01-29 20:33:18 -0600

    DescripcionInflables

--- inflatables.php
+++ inflatables.php
@@ -11,16 +11,19 @@
         <div class="container py-5">
             <div class="row mb-5">
                 <div class="col-md-8 col-xl-6 text-center mx-auto">
-                    <h2 class="fw-bold">Heading</h2>
-                    <p class="text-muted">Curae hendrerit donec commodo hendrerit egestas tempus, turpis facilisis nostra nunc. Vestibulum dui eget ultrices.</p>
+                    <h2 class="fw-bold">Inflatables</h2>
+                    <p class="text-muted">We supply professional services that simplify your entertainment needs, while making your experience unique. Our fun and courteous staff will help you with every aspect of your day, from setting up and supervising the fun, to tearing down at the end, making your event fun and hassle free!.</p>
                 </div>
             </div>
             <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/3a097840-d62f-4021-8cb3-265d3ac4fa3a.jpg" style="height: 250px;"></a>
                         <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <h4 class="fw-bold">Princess Bouncer</h4>
+                            <p class="text-muted">15x15 <br>
+                            The kingdom will gather to see this princess castle show a young prince and his queen to be.This bring colorful castle attractive whit its pink,blue and purple colors.It stand tall whit its brick and spear tower and inside large jumping area. This bouncer is sure to make any party a hit. 
+                        <br>Clean and sanitized .
+                        </p>
                         </div>
                     </div>
                 </div>
@@ -43,8 +46,10 @@
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/B37962D5-AEE8-4B6B-8E83-5736B1593FA9.jpg" style="height: 250px;"></a>
                         <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <h4 class="fw-bold">Blue Crush Water</h4>
+                            <p class="text-muted">L 32’(9.6m) <br>W 11’ (3.3m) <br>H 18 (5.4)<br>
+Our blue crush water slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
+<br>Clean and sanitized.</p>
                         </div>
                     </div>
                 </div>
@@ -59,24 +64,28 @@
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG_0854.png" style="height: 250px;"></a>
                         <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <h4 class="fw-bold">Dolphin Slide Pool</h4>
+                            <p class="text-muted">L 34ft. <br>  W 13ft. <br> H 18ft.<br>
+                            Our Dolphin Slide Poolslide slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
+<br>Clean and sanitized.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG_2392.jpg" style="height: 250px;"></a>
                         <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <h4 class="fw-bold">Rainbow Water Slide</h4>
+                            <p class="text-muted">16 ft <br>Our water slide wet and dry feature landing zone whit curved stop wall this half-pipe inspired design gives the rider a new experience as they come to a gradual stop after sliding up the back. Design whit safety and year round fun in mind,you all want at least one Wet and Dry in your party.
+  <br>Clean and sanitized </p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG-6097.jpg" style="height: 250px;"></a>
                         <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            <h4 class="fw-bold">Crayon Jumper</h4>
+                            <p class="text-muted">13x13 crayon jumper, our traditional castle bounce house bring fun to any party.</p>
                         </div>
                     </div>
                 </div>
