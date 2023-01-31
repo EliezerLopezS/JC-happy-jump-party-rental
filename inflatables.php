fe738fbae3829858e79c427c00e86419000efd4b
oswaldo5 <siete.o2r@gmail.com>
2023-01-30 23:05:15 -0600

    DescripcionInflables

--- inflatables.php
+++ inflatables.php
@@ -18,26 +18,28 @@
             <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/3a097840-d62f-4021-8cb3-265d3ac4fa3a.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Princess Bouncer</h4>
                             <p class="text-muted">15x15 <br>
-                            The kingdom will gather to see this princess castle show a young prince and his queen to be.This bring colorful castle attractive whit its pink,blue and purple colors.It stand tall whit its brick and spear tower and inside large jumping area. This bouncer is sure to make any party a hit. 
-                        <br>Clean and sanitized .
+                            The kingdom will gather to see this princess castle show a young prince and his queen to be. This bring colorful castle attractive whith its pink, blue and purple colors. It stand tall whit its brick, spear tower and inside large jumping area. This bouncer is sure to make any party a hit. 
+                        <br>Clean and sanitized.
                         </p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/4c02366c-ab97-4b00-99dd-7980ea02cc71.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Lorem libero donec</h4>
-                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
+                            <h4 class="fw-bold">Prince and Princess Castle</h4>
+                            <p class="text-muted">8.2L <br> 4.0W <br> 4.5h
+                            <br>This princess castle show a young prince and his queen to be. This bring colorful castle attractive whith its pink and purple colors.It stand tall whit its brick, spear tower and inside large jumping area. This bouncer is sure to make any party a hit.
+                                <br>Clean and sanitized.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
-                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/67726251116__922D4673-BF61-4213-A430-3799B6BF829B.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/IMG_6606.jpg" style="height: 250px;"></a>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Lorem libero donec</h4>
                             <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
                         </div>
@@ -45,47 +47,57 @@
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img\inflatables\IMG_9700.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Blue Crush Water</h4>
                             <p class="text-muted">L 32’(9.6m) <br>W 11’ (3.3m) <br>H 18 (5.4)<br>
-Our blue crush water slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
-<br>Clean and sanitized.</p>
+                            Our blue crush water slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
+                            <br>Clean and sanitized.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/IMG_6456.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Lorem libero donec</h4>
                             <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
-                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/IMG_0854.png" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
-                            <h4 class="fw-bold">Dolphin Slide Pool</h4>
-                            <p class="text-muted">L 34ft. <br>  W 13ft. <br> H 18ft.<br>
-                            Our Dolphin Slide Poolslide slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
-<br>Clean and sanitized.
+                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="Img/inflatables/tropical.jpg" style="height: 250px;"></a>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
+                            <h4 class="fw-bold">Blue Crust Water Slide</h4>
+                            <p class="text-muted">L 33ft. <br>  W 11ft. <br> H 18ft.<br>
+                            Our blue crush water slide whit inflatable pool.It’s a perfect slide for any event where folks to cool off and have that adrenaline pumping fun.
+                            <br>Clean and sanitized.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
                     <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG_2392.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Rainbow Water Slide</h4>
                             <p class="text-muted">16 ft <br>Our water slide wet and dry feature landing zone whit curved stop wall this half-pipe inspired design gives the rider a new experience as they come to a gradual stop after sliding up the back. Design whit safety and year round fun in mind,you all want at least one Wet and Dry in your party.
-  <br>Clean and sanitized </p>
+                            <br>Clean and sanitized </p>
                         </div>
                     </div>
                 </div>
                 <div class="col mb-4">
-                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG-6097.jpg" style="height: 250px;"></a>
-                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/crayon1.jpg" style="height: 250px;"></a>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
                             <h4 class="fw-bold">Crayon Jumper</h4>
                             <p class="text-muted">13x13 crayon jumper, our traditional castle bounce house bring fun to any party.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="img/inflatables/IMG-9094.jpg" style="height: 250px;"></a>
+                        <div class="py-4"><!--<span class="badge bg-primary mb-2">Website</span>-->
+                            <h4 class="fw-bold">Bahamas Breeze Combo 5-1</h4>
+                            <p class="text-muted"><br>L 33’ (10.1m)  <br>W 13’(3.9m) <br>H 12’(4.m)<br>
+Our Bahamas breeze combo inflatable is always a hit at any event,featuring a large bouncer area,basketball hoop and (inside), a challenging wall climb & a tall slide.Whit all these choices, kids of all ages will be engaged for hours.
+     <br>All the inflatables are clean and sanitized.</p>
                         </div>
                     </div>
                 </div>
